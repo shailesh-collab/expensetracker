9073a43a0c26ead5c7f8b4f039e0a35bd6d1d7c2
shailesh <shailesh@host.docker.internal>
2020-04-01 18:03:07 -0400

    test

--- expensetracker/src/main/java/com/example/expensetracker/ExpensetrackerApplication.java
+++ expensetracker/src/main/java/com/example/expensetracker/ExpensetrackerApplication.java
@@ -3,7 +3,7 @@ package com.example.expensetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test2
+//Teste
 @SpringBootApplication
 public class ExpensetrackerApplication {
 
